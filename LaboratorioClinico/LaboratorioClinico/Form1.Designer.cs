1d4938e65f6509056bccf9f1b586a0b62174809d
KariYos <yoskarinita@gmail.com>
2018-07-20 13:46:21 -0600

    LOGIN

--- LaboratorioClinico/LaboratorioClinico/Form1.Designer.cs
+++ LaboratorioClinico/LaboratorioClinico/Form1.Designer.cs
@@ -45,11 +45,11 @@
             // 
             // Btn_ingresar
             // 
-            this.Btn_ingresar.BackColor = System.Drawing.Color.Crimson;
+            this.Btn_ingresar.BackColor = System.Drawing.Color.Maroon;
             this.Btn_ingresar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.Btn_ingresar.Font = new System.Drawing.Font("Agency FB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Btn_ingresar.ForeColor = System.Drawing.Color.Thistle;
-            this.Btn_ingresar.Location = new System.Drawing.Point(145, 161);
+            this.Btn_ingresar.ForeColor = System.Drawing.Color.LightCoral;
+            this.Btn_ingresar.Location = new System.Drawing.Point(175, 169);
             this.Btn_ingresar.Name = "Btn_ingresar";
             this.Btn_ingresar.Size = new System.Drawing.Size(97, 29);
             this.Btn_ingresar.TabIndex = 0;
@@ -59,11 +59,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Perpetua", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Crimson;
-            this.label1.Location = new System.Drawing.Point(30, 83);
+            this.label1.Font = new System.Drawing.Font("Perpetua", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.PowderBlue;
+            this.label1.Location = new System.Drawing.Point(38, 83);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.Size = new System.Drawing.Size(75, 22);
             this.label1.TabIndex = 1;
             this.label1.Text = "Usuario";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -71,28 +71,28 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Perpetua", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.Crimson;
-            this.label2.Location = new System.Drawing.Point(30, 121);
+            this.label2.Font = new System.Drawing.Font("Perpetua", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.PowderBlue;
+            this.label2.Location = new System.Drawing.Point(38, 121);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(85, 18);
+            this.label2.Size = new System.Drawing.Size(103, 22);
             this.label2.TabIndex = 2;
             this.label2.Text = "Contraseña";
             // 
             // textBox1
             // 
-            this.textBox1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.textBox1.BackColor = System.Drawing.Color.LightBlue;
             this.textBox1.Font = new System.Drawing.Font("Perpetua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(122, 80);
+            this.textBox1.Location = new System.Drawing.Point(147, 83);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(149, 26);
             this.textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            this.textBox2.BackColor = System.Drawing.Color.LavenderBlush;
+            this.textBox2.BackColor = System.Drawing.Color.LightBlue;
             this.textBox2.Font = new System.Drawing.Font("Perpetua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(122, 118);
+            this.textBox2.Location = new System.Drawing.Point(147, 121);
             this.textBox2.Name = "textBox2";
             this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(149, 26);
@@ -111,27 +111,28 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Perpetua", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.Crimson;
-            this.label4.Location = new System.Drawing.Point(30, 47);
+            this.label4.Font = new System.Drawing.Font("Perpetua", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.PowderBlue;
+            this.label4.Location = new System.Drawing.Point(38, 47);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(76, 18);
+            this.label4.Size = new System.Drawing.Size(91, 22);
             this.label4.TabIndex = 6;
             this.label4.Text = "Privilegio";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // comboBox1
             // 
-            this.comboBox1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.comboBox1.BackColor = System.Drawing.Color.LightBlue;
             this.comboBox1.Font = new System.Drawing.Font("Perpetua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(122, 44);
+            this.comboBox1.Location = new System.Drawing.Point(147, 47);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(149, 26);
             this.comboBox1.TabIndex = 7;
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Location = new System.Drawing.Point(222, 35);
@@ -142,7 +143,7 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
             this.groupBox1.Controls.Add(this.comboBox1);
             this.groupBox1.Controls.Add(this.Btn_ingresar);
             this.groupBox1.Controls.Add(this.label1);
@@ -152,9 +153,9 @@
             this.groupBox1.Controls.Add(this.textBox2);
             this.groupBox1.Font = new System.Drawing.Font("Perpetua", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.ForeColor = System.Drawing.Color.MidnightBlue;
-            this.groupBox1.Location = new System.Drawing.Point(100, 132);
+            this.groupBox1.Location = new System.Drawing.Point(110, 132);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(332, 226);
+            this.groupBox1.Size = new System.Drawing.Size(327, 226);
             this.groupBox1.TabIndex = 9;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "LOGIN";
@@ -165,6 +166,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(543, 430);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
