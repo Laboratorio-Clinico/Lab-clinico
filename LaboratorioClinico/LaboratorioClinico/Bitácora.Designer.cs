7776fcb557c62adbdee8c9570af0cfd74b6ae3ec
Usuario <Usuario@INSPIRON>
2018-07-21 18:25:02 -0600

    Bitacora

--- LaboratorioClinico/LaboratorioClinico/Bitácora.Designer.cs
+++ LaboratorioClinico/LaboratorioClinico/Bitácora.Designer.cs
@@ -32,44 +32,44 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            this.button1 = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Btn_verb = new System.Windows.Forms.Button();
+            this.Gpb_bitacora = new System.Windows.Forms.GroupBox();
+            this.Dgv_bitacora = new System.Windows.Forms.DataGridView();
             this.Usuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.PC = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.label1 = new System.Windows.Forms.Label();
-            this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.Lbl_verb = new System.Windows.Forms.Label();
+            this.Gpb_bitacora.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_bitacora)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // Btn_verb
             // 
-            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button1.Location = new System.Drawing.Point(630, 88);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(53, 44);
-            this.button1.TabIndex = 9;
-            this.button1.UseVisualStyleBackColor = true;
+            this.Btn_verb.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Btn_verb.BackgroundImage")));
+            this.Btn_verb.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Btn_verb.Location = new System.Drawing.Point(630, 88);
+            this.Btn_verb.Name = "Btn_verb";
+            this.Btn_verb.Size = new System.Drawing.Size(53, 44);
+            this.Btn_verb.TabIndex = 9;
+            this.Btn_verb.UseVisualStyleBackColor = true;
             // 
-            // groupBox1
+            // Gpb_bitacora
             // 
-            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
-            this.groupBox1.Controls.Add(this.dataGridView1);
-            this.groupBox1.Font = new System.Drawing.Font("Perpetua", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.ForeColor = System.Drawing.Color.DarkKhaki;
-            this.groupBox1.Location = new System.Drawing.Point(64, 149);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(626, 196);
-            this.groupBox1.TabIndex = 10;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Bitacora Usuarios";
+            this.Gpb_bitacora.BackColor = System.Drawing.Color.Transparent;
+            this.Gpb_bitacora.Controls.Add(this.Dgv_bitacora);
+            this.Gpb_bitacora.Font = new System.Drawing.Font("Perpetua", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Gpb_bitacora.ForeColor = System.Drawing.Color.DarkKhaki;
+            this.Gpb_bitacora.Location = new System.Drawing.Point(64, 149);
+            this.Gpb_bitacora.Name = "Gpb_bitacora";
+            this.Gpb_bitacora.Size = new System.Drawing.Size(626, 196);
+            this.Gpb_bitacora.TabIndex = 10;
+            this.Gpb_bitacora.TabStop = false;
+            this.Gpb_bitacora.Text = "Bitacora Usuarios";
             // 
-            // dataGridView1
+            // Dgv_bitacora
             // 
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.Dgv_bitacora.BackgroundColor = System.Drawing.SystemColors.Control;
             dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
             dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
             dataGridViewCellStyle1.Font = new System.Drawing.Font("Perpetua", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -77,9 +77,9 @@
             dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Dgv_bitacora.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.Dgv_bitacora.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_bitacora.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Usuario,
             this.Column1,
             this.Column2,
@@ -91,9 +91,9 @@
             dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
-            this.dataGridView1.Location = new System.Drawing.Point(7, 34);
-            this.dataGridView1.Name = "dataGridView1";
+            this.Dgv_bitacora.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Dgv_bitacora.Location = new System.Drawing.Point(7, 34);
+            this.Dgv_bitacora.Name = "Dgv_bitacora";
             dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
             dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
             dataGridViewCellStyle3.Font = new System.Drawing.Font("Perpetua", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -101,9 +101,9 @@
             dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
-            this.dataGridView1.Size = new System.Drawing.Size(613, 141);
-            this.dataGridView1.TabIndex = 0;
+            this.Dgv_bitacora.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.Dgv_bitacora.Size = new System.Drawing.Size(613, 141);
+            this.Dgv_bitacora.TabIndex = 0;
             // 
             // Usuario
             // 
@@ -129,16 +129,16 @@
             this.PC.Name = "PC";
             this.PC.Width = 150;
             // 
-            // label1
+            // Lbl_verb
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Perpetua", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(627, 135);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(66, 15);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Ver Bitácora";
+            this.Lbl_verb.AutoSize = true;
+            this.Lbl_verb.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_verb.Font = new System.Drawing.Font("Perpetua", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_verb.Location = new System.Drawing.Point(627, 135);
+            this.Lbl_verb.Name = "Lbl_verb";
+            this.Lbl_verb.Size = new System.Drawing.Size(66, 15);
+            this.Lbl_verb.TabIndex = 1;
+            this.Lbl_verb.Text = "Ver Bitácora";
             // 
             // Bitácora
             // 
@@ -147,28 +147,28 @@
             this.BackgroundImage = global::LaboratorioClinico.Properties.Resources.FONDOO;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(766, 489);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Lbl_verb);
+            this.Controls.Add(this.Gpb_bitacora);
+            this.Controls.Add(this.Btn_verb);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Bitácora";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.groupBox1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Gpb_bitacora.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_bitacora)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Btn_verb;
+        private System.Windows.Forms.GroupBox Gpb_bitacora;
+        private System.Windows.Forms.DataGridView Dgv_bitacora;
         private System.Windows.Forms.DataGridViewTextBoxColumn Usuario;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn PC;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Lbl_verb;
     }
 }
