3c8d0deaf5c79ea10995622d7623d78787333f0d
Usuario <Usuario@INSPIRON>
2018-07-21 18:13:12 -0600

    Requisitos S

--- LaboratorioClinico/LaboratorioClinico/Requerimientos.Designer.cs
+++ LaboratorioClinico/LaboratorioClinico/Requerimientos.Designer.cs
@@ -29,32 +29,32 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Requerimientos));
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Gpb_llenador = new System.Windows.Forms.GroupBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.Txt_nombrer = new System.Windows.Forms.TextBox();
+            this.Lbl_nombrer = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
-            this.label4 = new System.Windows.Forms.Label();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.groupBox2.SuspendLayout();
-            this.groupBox3.SuspendLayout();
-            this.groupBox4.SuspendLayout();
+            this.Lbl_buscarr = new System.Windows.Forms.Label();
+            this.Lbl_imprimirr = new System.Windows.Forms.Label();
+            this.Gpb_fechar = new System.Windows.Forms.GroupBox();
+            this.Dtp_fechar = new System.Windows.Forms.DateTimePicker();
+            this.Lbl_fechar = new System.Windows.Forms.Label();
+            this.Gpb_imprimirr = new System.Windows.Forms.GroupBox();
+            this.Gpb_requisitos = new System.Windows.Forms.GroupBox();
+            this.Gpb_fechar.SuspendLayout();
+            this.Gpb_imprimirr.SuspendLayout();
+            this.Gpb_requisitos.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // Gpb_llenador
             // 
-            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
-            this.groupBox1.Font = new System.Drawing.Font("Perpetua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(94, 124);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(542, 239);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
+            this.Gpb_llenador.BackColor = System.Drawing.Color.Transparent;
+            this.Gpb_llenador.Font = new System.Drawing.Font("Perpetua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Gpb_llenador.Location = new System.Drawing.Point(94, 124);
+            this.Gpb_llenador.Name = "Gpb_llenador";
+            this.Gpb_llenador.Size = new System.Drawing.Size(542, 239);
+            this.Gpb_llenador.TabIndex = 0;
+            this.Gpb_llenador.TabStop = false;
             // 
             // button2
             // 
@@ -66,25 +66,26 @@
             this.button2.TabIndex = 4;
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // Txt_nombrer
             // 
-            this.textBox1.Font = new System.Drawing.Font("Perpetua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(208, 50);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(267, 26);
-            this.textBox1.TabIndex = 1;
+            this.Txt_nombrer.Font = new System.Drawing.Font("Perpetua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_nombrer.Location = new System.Drawing.Point(208, 50);
+            this.Txt_nombrer.Name = "Txt_nombrer";
+            this.Txt_nombrer.Size = new System.Drawing.Size(267, 26);
+            this.Txt_nombrer.TabIndex = 1;
             // 
-            // label1
+            // Lbl_nombrer
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Perpetua", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(71, 50);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(131, 22);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Nombre Examen";
+            this.Lbl_nombrer.AllowDrop = true;
+            this.Lbl_nombrer.AutoSize = true;
+            this.Lbl_nombrer.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_nombrer.Font = new System.Drawing.Font("Perpetua", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_nombrer.ForeColor = System.Drawing.Color.White;
+            this.Lbl_nombrer.Location = new System.Drawing.Point(71, 50);
+            this.Lbl_nombrer.Name = "Lbl_nombrer";
+            this.Lbl_nombrer.Size = new System.Drawing.Size(131, 22);
+            this.Lbl_nombrer.TabIndex = 2;
+            this.Lbl_nombrer.Text = "Nombre Examen";
             // 
             // button1
             // 
@@ -97,91 +98,91 @@
             this.button1.TabIndex = 3;
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // label2
+            // Lbl_buscarr
             // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Perpetua", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(531, 92);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 15);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Buscar";
+            this.Lbl_buscarr.AutoSize = true;
+            this.Lbl_buscarr.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_buscarr.Font = new System.Drawing.Font("Perpetua", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_buscarr.ForeColor = System.Drawing.Color.White;
+            this.Lbl_buscarr.Location = new System.Drawing.Point(531, 92);
+            this.Lbl_buscarr.Name = "Lbl_buscarr";
+            this.Lbl_buscarr.Size = new System.Drawing.Size(38, 15);
+            this.Lbl_buscarr.TabIndex = 4;
+            this.Lbl_buscarr.Text = "Buscar";
             // 
-            // label3
+            // Lbl_imprimirr
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Perpetua", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(208, 66);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 15);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Imprimir";
+            this.Lbl_imprimirr.AutoSize = true;
+            this.Lbl_imprimirr.Font = new System.Drawing.Font("Perpetua", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_imprimirr.ForeColor = System.Drawing.Color.White;
+            this.Lbl_imprimirr.Location = new System.Drawing.Point(208, 66);
+            this.Lbl_imprimirr.Name = "Lbl_imprimirr";
+            this.Lbl_imprimirr.Size = new System.Drawing.Size(48, 15);
+            this.Lbl_imprimirr.TabIndex = 5;
+            this.Lbl_imprimirr.Text = "Imprimir";
             // 
-            // groupBox2
+            // Gpb_fechar
             // 
-            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
-            this.groupBox2.Controls.Add(this.dateTimePicker2);
-            this.groupBox2.Controls.Add(this.label4);
-            this.groupBox2.Font = new System.Drawing.Font("Perpetua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox2.Location = new System.Drawing.Point(94, 369);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(268, 84);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            this.Gpb_fechar.BackColor = System.Drawing.Color.Transparent;
+            this.Gpb_fechar.Controls.Add(this.Dtp_fechar);
+            this.Gpb_fechar.Controls.Add(this.Lbl_fechar);
+            this.Gpb_fechar.Font = new System.Drawing.Font("Perpetua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Gpb_fechar.Location = new System.Drawing.Point(94, 369);
+            this.Gpb_fechar.Name = "Gpb_fechar";
+            this.Gpb_fechar.Size = new System.Drawing.Size(268, 84);
+            this.Gpb_fechar.TabIndex = 1;
+            this.Gpb_fechar.TabStop = false;
+            this.Gpb_fechar.Enter += new System.EventHandler(this.groupBox2_Enter);
             // 
-            // dateTimePicker2
+            // Dtp_fechar
             // 
-            this.dateTimePicker2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.Dtp_fechar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dateTimePicker2.CustomFormat = "dd-MM-yy";
-            this.dateTimePicker2.DropDownAlign = System.Windows.Forms.LeftRightAlignment.Right;
-            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker2.Location = new System.Drawing.Point(132, 35);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(104, 26);
-            this.dateTimePicker2.TabIndex = 22;
+            this.Dtp_fechar.CustomFormat = "dd-MM-yy";
+            this.Dtp_fechar.DropDownAlign = System.Windows.Forms.LeftRightAlignment.Right;
+            this.Dtp_fechar.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.Dtp_fechar.Location = new System.Drawing.Point(132, 35);
+            this.Dtp_fechar.Name = "Dtp_fechar";
+            this.Dtp_fechar.Size = new System.Drawing.Size(104, 26);
+            this.Dtp_fechar.TabIndex = 22;
             // 
-            // label4
+            // Lbl_fechar
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Perpetua", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(18, 41);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(107, 22);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "Fecha emisión";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
+            this.Lbl_fechar.AutoSize = true;
+            this.Lbl_fechar.Font = new System.Drawing.Font("Perpetua", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_fechar.ForeColor = System.Drawing.Color.White;
+            this.Lbl_fechar.Location = new System.Drawing.Point(18, 41);
+            this.Lbl_fechar.Name = "Lbl_fechar";
+            this.Lbl_fechar.Size = new System.Drawing.Size(107, 22);
+            this.Lbl_fechar.TabIndex = 5;
+            this.Lbl_fechar.Text = "Fecha emisión";
+            this.Lbl_fechar.Click += new System.EventHandler(this.label4_Click);
             // 
-            // groupBox3
+            // Gpb_imprimirr
             // 
-            this.groupBox3.BackColor = System.Drawing.Color.Transparent;
-            this.groupBox3.Controls.Add(this.button2);
-            this.groupBox3.Controls.Add(this.label3);
-            this.groupBox3.Font = new System.Drawing.Font("Perpetua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox3.Location = new System.Drawing.Point(368, 369);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(268, 84);
-            this.groupBox3.TabIndex = 23;
-            this.groupBox3.TabStop = false;
+            this.Gpb_imprimirr.BackColor = System.Drawing.Color.Transparent;
+            this.Gpb_imprimirr.Controls.Add(this.button2);
+            this.Gpb_imprimirr.Controls.Add(this.Lbl_imprimirr);
+            this.Gpb_imprimirr.Font = new System.Drawing.Font("Perpetua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Gpb_imprimirr.Location = new System.Drawing.Point(368, 369);
+            this.Gpb_imprimirr.Name = "Gpb_imprimirr";
+            this.Gpb_imprimirr.Size = new System.Drawing.Size(268, 84);
+            this.Gpb_imprimirr.TabIndex = 23;
+            this.Gpb_imprimirr.TabStop = false;
             // 
-            // groupBox4
+            // Gpb_requisitos
             // 
-            this.groupBox4.BackColor = System.Drawing.Color.Transparent;
-            this.groupBox4.Controls.Add(this.label1);
-            this.groupBox4.Controls.Add(this.textBox1);
-            this.groupBox4.Font = new System.Drawing.Font("Perpetua", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox4.ForeColor = System.Drawing.Color.DarkKhaki;
-            this.groupBox4.Location = new System.Drawing.Point(31, 12);
-            this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(723, 465);
-            this.groupBox4.TabIndex = 24;
-            this.groupBox4.TabStop = false;
-            this.groupBox4.Text = "Requisitos";
+            this.Gpb_requisitos.BackColor = System.Drawing.Color.Transparent;
+            this.Gpb_requisitos.Controls.Add(this.Lbl_nombrer);
+            this.Gpb_requisitos.Controls.Add(this.Txt_nombrer);
+            this.Gpb_requisitos.Font = new System.Drawing.Font("Perpetua", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Gpb_requisitos.ForeColor = System.Drawing.Color.DarkKhaki;
+            this.Gpb_requisitos.Location = new System.Drawing.Point(31, 12);
+            this.Gpb_requisitos.Name = "Gpb_requisitos";
+            this.Gpb_requisitos.Size = new System.Drawing.Size(723, 465);
+            this.Gpb_requisitos.TabIndex = 24;
+            this.Gpb_requisitos.TabStop = false;
+            this.Gpb_requisitos.Text = "Requisitos";
             // 
             // Requerimientos
             // 
@@ -190,23 +191,23 @@
             this.BackgroundImage = global::LaboratorioClinico.Properties.Resources.FONDOO;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(766, 489);
-            this.Controls.Add(this.groupBox3);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Gpb_imprimirr);
+            this.Controls.Add(this.Gpb_fechar);
+            this.Controls.Add(this.Lbl_buscarr);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.Gpb_llenador);
+            this.Controls.Add(this.Gpb_requisitos);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Requerimientos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.Requerimientos_Load);
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
-            this.groupBox3.ResumeLayout(false);
-            this.groupBox3.PerformLayout();
-            this.groupBox4.ResumeLayout(false);
-            this.groupBox4.PerformLayout();
+            this.Gpb_fechar.ResumeLayout(false);
+            this.Gpb_fechar.PerformLayout();
+            this.Gpb_imprimirr.ResumeLayout(false);
+            this.Gpb_imprimirr.PerformLayout();
+            this.Gpb_requisitos.ResumeLayout(false);
+            this.Gpb_requisitos.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -214,17 +215,17 @@
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox Gpb_llenador;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Txt_nombrer;
+        private System.Windows.Forms.Label Lbl_nombrer;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.DateTimePicker dateTimePicker2;
-        private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox Gpb_fechar;
+        private System.Windows.Forms.Label Lbl_buscarr;
+        private System.Windows.Forms.Label Lbl_imprimirr;
+        private System.Windows.Forms.Label Lbl_fechar;
+        private System.Windows.Forms.DateTimePicker Dtp_fechar;
+        private System.Windows.Forms.GroupBox Gpb_imprimirr;
+        private System.Windows.Forms.GroupBox Gpb_requisitos;
     }
 }
