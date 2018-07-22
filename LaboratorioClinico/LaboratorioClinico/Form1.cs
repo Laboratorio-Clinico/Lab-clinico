8f25b8b9db0eba01ce63e6cf8b2356cb2d5e47ef
KariYos <yoskarinita@gmail.com>
2018-07-21 18:25:16 -0600

    login

--- LaboratorioClinico/LaboratorioClinico/Form1.cs
+++ LaboratorioClinico/LaboratorioClinico/Form1.cs
@@ -60,24 +60,10 @@ namespace LaboratorioClinico
 
             string user = Txt_usuario.Text;
             string pass = Txt_contrasena.Text;
-            int privi = 0;
+   
 
 
-
-            if (String.Compare(Cmb_privilegio.SelectedItem.ToString(), "Administrador") == 0)
-            {
-                privi = 1;
-            }
-            else if (String.Compare(Cmb_privilegio.SelectedItem.ToString(), "UsuarioL") == 0)
-            {
-                privi = 2;
-            }
-            else if (String.Compare(Cmb_privilegio.SelectedItem.ToString(), "Usuario") == 0)
-            {
-                privi = 3;
-            }
-
-            MySqlDataAdapter sda = new MySql.Data.MySqlClient.MySqlDataAdapter("select count(*) from usuario where usuario='" + user + "'and contraseña ='" + pass + "'and idprivilegio='" + privi + "'", conexion.ObtenerConexion());
+            MySqlDataAdapter sda = new MySql.Data.MySqlClient.MySqlDataAdapter("select count(*) from usuario where usuario='" + user + "'and contraseña ='" + pass, conexion.ObtenerConexion());
             MySqlCommand cmd = conexion.ObtenerConexion().CreateCommand();
             DataTable datos = new DataTable();
             sda.Fill(datos);
@@ -90,10 +76,10 @@ namespace LaboratorioClinico
 
                 Txt_usuario.ResetText();
                 Txt_contrasena.ResetText();
-                Cmb_privilegio.ResetText();
+     
 
                 this.Hide();
-                Menu abrir = new Menu(Txt_usuario.Text, Cmb_privilegio.SelectedItem.ToString());
+                Menu abrir = new Menu(Txt_usuario.Text);
                 abrir.ShowDialog();
                 this.Show();
 
