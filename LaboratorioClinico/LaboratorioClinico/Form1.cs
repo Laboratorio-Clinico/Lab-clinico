0346bda8e5b6e10ca67d87216128439489dbd641
Julio Sicaja <elmasterrr000@gmail.com>
2018-07-21 19:14:29 -0600

    Karina

--- LaboratorioClinico/LaboratorioClinico/Form1.cs
+++ LaboratorioClinico/LaboratorioClinico/Form1.cs
@@ -68,7 +68,7 @@ namespace LaboratorioClinico
             DataTable datos = new DataTable();
             sda.Fill(datos);
 
-            if (datos.Rows[0][0].ToString() == "Administrador")
+            if (datos.Rows[0][0].ToString() == Convert.ToString("1"))
             {
                 MessageBox.Show("Usuario Correcto", "Correcto", MessageBoxButtons.OK, MessageBoxIcon.Information);
            
