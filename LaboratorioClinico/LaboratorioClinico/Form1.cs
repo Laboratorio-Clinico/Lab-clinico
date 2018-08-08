e6b8d72bfc0cb8b967266425fca8803d726ac60c
KariYos <yoskarinita@gmail.com>
2018-08-07 22:42:00 -0600

    login

--- LaboratorioClinico/LaboratorioClinico/Form1.cs
+++ LaboratorioClinico/LaboratorioClinico/Form1.cs
@@ -57,8 +57,7 @@ namespace LaboratorioClinico
             BorrarMsj();
             Validar();
             string user = Txt_usuario.Text;
-            //string pass = EncripContra(Txt_contraseña.Text);
-            string pass = Txt_contraseña.Text;
+            string pass = EncripContra(Txt_contraseña.Text);
             string tipo;
 
 
