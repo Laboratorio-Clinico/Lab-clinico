08ef47ae5fb29901a132dc7ab2ccf4d397aa020d
SindyBatz <sindybatz02082014@gmail.com>
2018-08-08 01:09:31 -0600

    .

--- LaboratorioClinico/LaboratorioClinico/Constancia.cs
+++ LaboratorioClinico/LaboratorioClinico/Constancia.cs
@@ -37,24 +37,16 @@ namespace LaboratorioClinico
         {
             try
             {
-                OdbcDataAdapter sda = new OdbcDataAdapter("SELECT sNombre, sNit, sDireccion FROM paciente WHERE nIdPaciente ='" + Convert.ToInt64(Txt_dpir.Text) + "'", conexion.ObtenerConexion());
+                OdbcDataAdapter sda = new OdbcDataAdapter("SELECT pa.sNombre, pa.sNit, pa. sDireccion, ci.iIdCitas,ci.dFecha  FROM paciente pa,citas ci WHERE nIdPaciente ='" + Convert.ToInt64(Txt_dpir.Text) + "'", conexion.ObtenerConexion());
                 DataTable datos = new DataTable();
                 sda.Fill(datos);
-
 
                 Txt_nombrer.Text = datos.Rows[0][0].ToString();
                 Txt_direccionr.Text = datos.Rows[0][1].ToString();
                 Txt_nitc.Text = datos.Rows[0][2].ToString();
-
-                OdbcDataAdapter sda2 = new OdbcDataAdapter("SELECT iIdCitas, dFecha FROM citas WHERE nNoExpediente ='" + Convert.ToInt64(Txt_dpir.Text) + "'", conexion.ObtenerConexion());
-                DataTable datos2 = new DataTable();
-                sda2.Fill(datos2);
-
-                Txt_cita.Text = datos2.Rows[0][0].ToString();
-                Txt_fechae.Text = datos2.Rows[0][1].ToString();
-
-
-
+                Txt_cita.Text = datos.Rows[0][3].ToString();
+                Txt_fechae.Text = datos.Rows[0][4].ToString();
+      
             }
             catch (Exception ex)
             {
